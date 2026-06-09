df035aa07efd8e9c8514325490ea4a4d3b713d2f
Woohyuck Jeong <jwhyukk10@gmail.com>
2026-06-09 19:16:14 +0900

    Save v1 compare result

--- results/plot_bench_res2.py
+++ results/plot_bench_res2.py
@@ -7,7 +7,8 @@ from glob import glob
 dataDir = "/Users/woohyuck/Documents/02_workspace/04_Lecture/01_Mobility/MyProject/term_project/benchmark"
 dataList = glob(os.path.join(dataDir, "*.csv"))
 
-rawDataIdx = [2, 4, 0, 1]
+# rawDataIdx = [2, 4, 0, 1]
+rawDataIdx = [2, 4, 0]
 rawDataList = [dataList[i] for i in rawDataIdx]
 
 rawData = [pd.read_csv(f) for f in rawDataList]
@@ -15,7 +16,7 @@ rawData = [pd.read_csv(f) for f in rawDataList]
 rrt_star = rawData[0].copy()
 informed_rrt_star = rawData[1].copy()
 neural_rrt_v1 = rawData[2].copy()
-neural_rrt_v2 = rawData[3].copy()
+# neural_rrt_v2 = rawData[3].copy()
 
 save_dir = "./results"
 os.makedirs(save_dir, exist_ok=True)
@@ -24,12 +25,12 @@ algorithms = [
     (rrt_star, "rrt*", "red", "+"),
     (informed_rrt_star, "Informed RRT*", "blue", "^"),
     (neural_rrt_v1, "Neural RRT* v1", "green", "d"),
-    (neural_rrt_v2, "Neural RRT* v2", "orange", "o"),
+    # (neural_rrt_v2, "Neural RRT* v2", "orange", "o"),
 ]
 
 
 def save_plot(y_col, title, filename, ylim=None):
-    fig, ax = plt.subplots(figsize=(12, 6))
+    fig, ax = plt.subplots(figsize=(8, 6))
 
     for df, label, color, marker in algorithms:
         df.plot(
@@ -62,7 +63,7 @@ def save_plot(y_col, title, filename, ylim=None):
 save_plot(
     y_col="first_path_iter",
     title="First Path Iter",
-    filename="first_path_iter.png",
+    filename="first_path_iter_v1.png",
     ylim=(0, 500)
 )
 
@@ -70,14 +71,14 @@ save_plot(
 save_plot(
     y_col="final_cost",
     title="Final Cost",
-    filename="final_cost.png"
+    filename="final_cost_v1.png"
 )
 
 # 3. final_nodes
 save_plot(
     y_col="final_nodes",
     title="Final Nodes",
-    filename="final_nodes.png",
+    filename="final_nodes_v1.png",
     ylim=(1700, 2100)
 )
 
@@ -85,7 +86,7 @@ save_plot(
 save_plot(
     y_col="elapsed_sec",
     title="Elapsed Time [s]",
-    filename="elapsed_sec.png"
+    filename="elapsed_sec_v1.png"
 )
 
 print("All plots saved.")
